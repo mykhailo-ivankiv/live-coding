a28b7d2c7b52245fa5ad132e5e0e5b6beab7c6d2
mykhailo.ivankiv <mykhailo.ivankiv@contextlabs.com>
2023-03-16 12:28:49 +0100

    Remove nodes

---
+++ apps/pipeline-editor/src/components/Node.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { Matrix } from 'transformation-matrix'
+import { DragState } from '@vueuse/gesture'
+
+const props = defineProps<{
+  node: Node
+  matrix: Matrix
+  isSelected: boolean
+}>()
+
+const nodeDragHandler =
+  (node) =>
+  ({ delta }: DragState) => {
+    node.position.x += (delta[0] * 1) / props.matrix.a
+    node.position.y += (delta[1] * 1) / props.matrix.a
+  }
+</script>
+
+<template>
+  <div
+    v-drag="nodeDragHandler(node)"
+    class="absolute z-20"
+    :key="node.id"
+    :style="`
+          left: ${node.position.x}px;
+          top: ${node.position.y}px;
+          width: ${node.position.width}px;
+          height: ${node.position.height}px;
+        `"
+  >
+    <button
+      v-if="node.type === 'data'"
+      class="absolute top-1/2 -right-5 transform -translate-y-1/2"
+      @click="addDataNode(node)"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 fill-white"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </button>
+    <div
+      class="h-32 w-96 box-border rounded-md overflow-hidden border-2 bg-white flex flex-col"
+      :class="isSelected ? 'border-blue-700 shadow shadow-blue-300' : 'border-gray-700'"
+    >
+      <div class="py-1 px-2 pl-4 border-b-2">title</div>
+      <!--
+      Using css transform results in incorrect tooltip positioning
+      https://github.com/codemirror/dev/issues/324
+    -->
+      <iframe v-if="node.type === 'data'" class="h-full w-full" :src="`/edit/${node.source}`" />
+      <iframe v-else-if="node.type === 'cache'" class="h-full w-full" :src="`/view/${node.source}`" />
+      <iframe v-else class="h-full w-full" :src="`/edit/${node.source}`" />
+      <!-- <JsonEditor v-if="node?.type === 'data'" />-->
+      <!-- <JavascriptEditor v-else-if="node?.type === 'function'" />-->
+    </div>
+  </div>
+</template>
--- apps/pipeline-editor/src/components/PipelineEditor.vue
+++ apps/pipeline-editor/src/components/PipelineEditor.vue
@@ -2,15 +2,21 @@
 import { translate, toCSS, compose, scale, inverse, applyToPoint } from 'transformation-matrix'
 import { computed, ref } from 'vue'
 import Edge from './Edge.vue'
+import Node from './Node.vue'
 import { findYPosition, isRectsIntersect, Rect } from '../utils/geomenty'
 import { DragState } from '@vueuse/gesture'
+
+type Rectangle = { x: number; y: number; width: number; height: number }
+type Node = { id: string; source: string; type: 'data' | 'function' | 'cache'; position: Rectangle; title: string }
+type Edge = { id: string; source: string; target: string }
+type Pipeline = { id: string; nodes: Node[]; edges: Edge[] }
 
 const canvas = ref(null)
 const root = ref(null)
 const matrix = ref(translate(400, 0))
 const inverseMatrix = computed(() => inverse(matrix.value))
 
-const pipeline = await (await fetch('http://localhost:3000/pipelines/1')).json()
+const pipeline: Pipeline = await (await fetch('http://localhost:3000/pipelines/1')).json()
 
 const nodes = ref(
   pipeline.nodes.reduce((acc, node) => {
@@ -20,6 +26,16 @@ const nodes = ref(
 )
 
 const edges = ref(pipeline.edges)
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Delete' || e.key === 'Backspace') {
+    selectedNodes.value.forEach((id) => {
+      delete nodes.value[id]
+      edges.value = edges.value.filter((edge) => edge.source !== id && edge.target !== id)
+    })
+    selectedNodes.value = []
+  }
+})
 
 const handleCanvasDrag = ({ delta: [x, y] }: DragState) => {
   // Do something with dragState
@@ -80,13 +96,6 @@ const canvasZoomHandler = ({ event, delta }) => {
 
 const selection = ref<Rect | null>(null)
 
-const nodeDragHandler =
-  (node) =>
-  ({ delta }: DragState) => {
-    node.position.x += (delta[0] * 1) / matrix.value.a
-    node.position.y += (delta[1] * 1) / matrix.value.a
-  }
-
 const addDataNode = (node) => {
   const width = 380
   const height = 124
@@ -137,54 +146,7 @@ const addDataNode = (node) => {
         />
       </svg>
 
-      <!-- Nodes -->
-      <div
-        v-for="node in nodes"
-        v-drag="nodeDragHandler(nodes[node.id])"
-        class="absolute z-20"
-        :key="node.id"
-        :style="`
-          left: ${node.position.x}px;
-          top: ${node.position.y}px;
-          width: ${node.position.width}px;
-          height: ${node.position.height}px;
-        `"
-      >
-        <button
-          v-if="node.type === 'data'"
-          class="absolute top-1/2 -right-5 transform -translate-y-1/2"
-          @click="addDataNode(node)"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 fill-white"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </button>
-        <div
-          class="h-32 w-96 box-border rounded-md overflow-hidden border-2 bg-white flex flex-col"
-          :class="selectedNodes.includes(node.id) ? 'border-blue-700 shadow shadow-blue-300' : 'border-gray-700'"
-        >
-          <div class="py-1 px-2 pl-4 border-b-2">title</div>
-          <!--
-          Using css transform results in incorrect tooltip positioning
-          https://github.com/codemirror/dev/issues/324
-        -->
-          <iframe v-if="node.type === 'data'" class="h-full w-full" :src="`/edit/${node.source}`" />
-          <iframe v-else-if="node.type === 'cache'" class="h-full w-full" :src="`/view/${node.source}`" />
-          <iframe v-else class="h-full w-full" :src="`/edit/${node.source}`" />
-          <!-- <JsonEditor v-if="node?.type === 'data'" />-->
-          <!-- <JavascriptEditor v-else-if="node?.type === 'function'" />-->
-        </div>
-      </div>
+      <Node v-for="node in nodes" :node="node" :matrix="matrix" :isSelected="selectedNodes.includes(node.id)" />
     </div>
   </div>
 </template>
